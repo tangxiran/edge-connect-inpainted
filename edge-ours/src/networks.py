825825272655b728ae61aec0280b4d278a0a5e3d
wangxutao <291477206@qq.com>
2020-10-30 13:24:16 +0800

    add my net

---
+++ edge-ours/src/networks.py
@@ -0,0 +1,266 @@
+import torch
+import torch.nn as nn
+import torchsummary
+
+class BaseNetwork(nn.Module):
+    def __init__(self):
+        super(BaseNetwork, self).__init__()
+
+    def init_weights(self, init_type='normal', gain=0.02):
+        '''
+        initialize network's weights
+        init_type: normal | xavier | kaiming | orthogonal
+        https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/9451e70673400885567d08a9e97ade2524c700d0/models/networks.py#L39
+        '''
+
+        def init_func(m):
+            classname = m.__class__.__name__
+            if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+                if init_type == 'normal':
+                    nn.init.normal_(m.weight.data, 0.0, gain)
+                elif init_type == 'xavier':
+                    nn.init.xavier_normal_(m.weight.data, gain=gain)
+                elif init_type == 'kaiming':
+                    nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+                elif init_type == 'orthogonal':
+                    nn.init.orthogonal_(m.weight.data, gain=gain)
+
+                if hasattr(m, 'bias') and m.bias is not None:
+                    nn.init.constant_(m.bias.data, 0.0)
+
+            elif classname.find('BatchNorm2d') != -1:
+                nn.init.normal_(m.weight.data, 1.0, gain)
+                nn.init.constant_(m.bias.data, 0.0)
+
+        self.apply(init_func)
+
+
+class InpaintGenerator(BaseNetwork):
+    def __init__(self, residual_blocks=8, init_weights=True):
+        super(InpaintGenerator, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(in_channels=4, out_channels=64, kernel_size=7, padding=0),
+            nn.InstanceNorm2d(64, track_running_stats=False),
+            nn.ReLU(True),
+
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(128, track_running_stats=False),
+            nn.ReLU(True),
+
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(256, track_running_stats=False),
+            nn.ReLU(True)
+        )
+
+        blocks = []
+        for _ in range(residual_blocks):
+            block = ResnetBlock(dim=256,dilation=2)
+            # block = ResnetBlock(256,2)
+            # wxt修改的
+            blocks.append(block)
+
+        self.middle = nn.Sequential(*blocks)
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(128, track_running_stats=False),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(64, track_running_stats=False),
+            nn.ReLU(True),
+
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(in_channels=64, out_channels=3, kernel_size=7, padding=0),
+        )
+
+        if init_weights:
+            self.init_weights()
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.middle(x)
+        x = self.decoder(x)
+        x = (torch.tanh(x) + 1) / 2
+
+        return x
+
+
+class EdgeGenerator(BaseNetwork):
+    def __init__(self, residual_blocks=8, use_spectral_norm=True, init_weights=True):
+        super(EdgeGenerator, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.ReflectionPad2d(3),
+            spectral_norm(nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, padding=0), use_spectral_norm),
+            nn.InstanceNorm2d(64, track_running_stats=False),
+            nn.ReLU(True),
+
+            spectral_norm(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1), use_spectral_norm),
+            nn.InstanceNorm2d(128, track_running_stats=False),
+            nn.ReLU(True),
+
+            spectral_norm(nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1), use_spectral_norm),
+            nn.InstanceNorm2d(256, track_running_stats=False),
+            nn.ReLU(True)
+        )
+
+        blocks = []
+        for _ in range(residual_blocks):
+            block = ResnetBlock(dim=256, dilation = 1, use_spectral_norm=use_spectral_norm)
+            # wxt修改的，改爲1  2020-9-22
+            # block = ResnetBlock(dim=256, dilation=2, use_spectral_norm=use_spectral_norm)
+            blocks.append(block)
+
+        self.middle = nn.Sequential(*blocks)
+
+        self.decoder = nn.Sequential(
+            spectral_norm(nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1), use_spectral_norm),
+            nn.InstanceNorm2d(128, track_running_stats=False),
+            nn.ReLU(True),
+
+            spectral_norm(nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1), use_spectral_norm),
+            nn.InstanceNorm2d(64, track_running_stats=False),
+            nn.ReLU(True),
+
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(in_channels=64, out_channels=1, kernel_size=7, padding=0),
+        )
+
+
+        if init_weights:
+            self.init_weights()
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.middle(x)
+        x = self.decoder(x)
+        x = torch.sigmoid(x)
+        return x
+
+
+class Discriminator(BaseNetwork):
+    def __init__(self, in_channels, use_sigmoid=True, use_spectral_norm=True, init_weights=True):
+        super(Discriminator, self).__init__()
+        self.use_sigmoid = use_sigmoid
+
+        self.conv1 = self.features = nn.Sequential(
+            spectral_norm(nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=4, stride=2, padding=1, bias=not use_spectral_norm), use_spectral_norm),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.conv2 = nn.Sequential(
+            spectral_norm(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1, bias=not use_spectral_norm), use_spectral_norm),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.conv3 = nn.Sequential(
+            spectral_norm(nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1, bias=not use_spectral_norm), use_spectral_norm),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.conv4 = nn.Sequential(
+            spectral_norm(nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=1, padding=1, bias=not use_spectral_norm), use_spectral_norm),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.conv5 = nn.Sequential(
+            spectral_norm(nn.Conv2d(in_channels=512, out_channels=1, kernel_size=4, stride=1, padding=1, bias=not use_spectral_norm), use_spectral_norm),
+        )
+
+        if init_weights:
+            self.init_weights()
+
+    def forward(self, x):
+        conv1 = self.conv1(x)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+        conv4 = self.conv4(conv3)
+        conv5 = self.conv5(conv4)
+
+        outputs = conv5
+        if self.use_sigmoid:
+            outputs = torch.sigmoid(conv5)
+
+        return outputs, [conv1, conv2, conv3, conv4, conv5]
+
+
+class ResnetBlock(nn.Module):
+    def __init__(self, dim, dilation=1, use_spectral_norm=False):
+        super(ResnetBlock, self).__init__()
+        self.conv_block = nn.Sequential(
+            nn.ReflectionPad2d(dilation), # reflection镜像填充
+            spectral_norm(nn.Conv2d(in_channels=dim, out_channels=dim,
+                                    kernel_size=3, padding=0, dilation=dilation, bias=not use_spectral_norm), use_spectral_norm),
+            nn.InstanceNorm2d(dim, track_running_stats=False),
+            nn.ReLU(True),
+
+            nn.ReflectionPad2d(1),
+            spectral_norm(nn.Conv2d(in_channels=dim, out_channels=dim,
+                                    kernel_size=3, padding=0, dilation=1, bias=not use_spectral_norm), use_spectral_norm),
+            nn.InstanceNorm2d(dim, track_running_stats=False),
+        )
+
+    def forward(self, x):
+        out = x + self.conv_block(x)
+
+        # Remove ReLU at the end of the residual block
+        # http://torch.ch/blog/2016/02/04/resnets.html
+
+        return out
+
+
+def spectral_norm(module, mode=True):
+    if mode:
+        return nn.utils.spectral_norm(module)
+
+    return module
+
+if __name__ == '__main__':
+    import torch
+    from torchvision import models
+    from torchsummary import summary
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    modell =EdgeGenerator().to(device)
+    summayy= summary(modell, (3, 128, 128))
+    print(summayy )
+
+
+
+   #################################
+    from torch.utils.tensorboard import SummaryWriter
+
+    net = modell
+    print(net)
+    print(summary(net, (3, 128, 128))
+          )
+    # writer = SummaryWriter('./data/tensorboard')
+    # writer.add_graph(net, input_to_model=torch.rand(1, 3, 32, 32))
+    # writer.close()
+
+    from tensorboard import notebook
+
+    # 等价于在命令行中执行 tensorboard --logdir ./data/tensorboard
+    # 可以在浏览器中打开 http://localhost:6006/ 查看
+    '''
+    有时候在训练过程中，如果能够实时动态地查看loss和各种metric的变化曲线，那么无疑可以帮助我们更加直观地了解模型的训练情况。
+    
+    注意，writer.add_scalar仅能对标量的值的变化进行可视化。因此它一般用于对loss和metric的变化进行可视化分析
+    '''
+    # 二，可视化指标变化
+
+    # 写入模型结构
+
+    writer = SummaryWriter('./data/tensorboard')
+    input_data  = (torch.rand(1,3,128,128)).to(device)
+    writer.add_graph(net, input_to_model=input_data)
+    writer.close()
+    # 查看模型结构
+    from tensorboard import notebook
+
+    #
+    notebook.list()
+    notebook.start("--logdir ./data/tensorboard")
